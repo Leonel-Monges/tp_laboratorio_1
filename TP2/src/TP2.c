8ecc14cf5430f7571888ba0abce83fd425dd3af8
Leonel Monges <lnm.lambda@gmail.com>
2022-04-18 11:40:49 -0300

    TP2

---
+++ TP2/src/TP2.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : TP2.c
+ Author      : Leonel Monges
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN_NAME 51
+#define LEN_FLYCODE 10
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+//	int id;
+//	char name[LEN_NAME];
+//	char lastname[LEN_NAME];
+//	float price;
+//	char flycode[LEN_FLYCODE];
+//	int typePassenger;
+//	int statusFlight;
+//	int isEmpty;
+
+	printf(" TP2 en proceso!");
+
+	return EXIT_SUCCESS;
+}
---
+++ TP2/src/arrayPassenger.h
@@ -0,0 +1,13 @@
+/*
+ * arrayPassenger.h
+ *
+ *  Created on: Apr 17, 2022
+ *      Author: Leo
+ */
+
+#ifndef ARRAYPASSENGER_H_
+#define ARRAYPASSENGER_H_
+
+
+
+#endif /* ARRAYPASSENGER_H_ */
